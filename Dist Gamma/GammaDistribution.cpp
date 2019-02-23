c6a0ad11bde643be5d121153d386a400a3029cd5
Eduardo Larios <eduardo.larios.fdz@outlook.com>
2019-02-23 16:34:46 -0600

    working on gamma distribution

--- Dist Gamma/GammaDistribution.cpp
+++ Dist Gamma/GammaDistribution.cpp
@@ -1,4 +1,4 @@
-// weibull_distribution
+// gamma_distribution
 #include <iostream>
 #include <random>
 #include <string>
@@ -7,27 +7,26 @@ using namespace std;
 
 int main()
 {
-    const int nstars = 100;   // maximum number of stars to distribute
     int i, iterations;
+    const int nstars = 100; // maximum number of stars to distribute
 
     default_random_engine generator;
-    weibull_distribution<double> distribution(2.0, 4.0);
-
-    int p[10] = {};
+    gamma_distribution<double> distribution(2.0, 2.0);
 
     printf("Numbers to be generated: ");
     scanf("%d", &iterations);
 
+    int p[10] = {};
+
     for (int i = 0; i < iterations; ++i)
     {
         double number = distribution(generator);
-        printf("%i): %f\n",i ,number);
-
+        printf("%i): %f\n", i, number);
         if (number < 10)
             ++p[int(number)];
     }
 
-    cout << "\nWeibull Distribution Plot:" << endl;
+    cout << "\nGamma Distribution Plot:" << endl;
 
     for (int i = 0; i < 10; ++i)
     {
