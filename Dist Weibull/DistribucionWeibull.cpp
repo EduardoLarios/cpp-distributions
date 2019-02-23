7272d69c9a7788f62a0230d783abbb48a1a13fa3
Eduardo Larios <eduardo.larios.fdz@outlook.com>
2019-02-23 15:29:39 -0600

    added weibull distribution

--- Dist Weibull/DistribucionWeibull.cpp
+++ Dist Weibull/DistribucionWeibull.cpp
@@ -1,32 +1,38 @@
 // weibull_distribution
 #include <iostream>
 #include <random>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    const int nrolls = 10000; // number of experiments
     const int nstars = 100;   // maximum number of stars to distribute
+    int digits, i, iterations;
 
     default_random_engine generator;
     weibull_distribution<double> distribution(2.0, 4.0);
 
     int p[10] = {};
 
-    for (int i = 0; i < nrolls; ++i)
+    printf("Numbers to be generated: ");
+    scanf("%d", &iterations);
+
+    for (int i = 0; i < iterations; ++i)
     {
         double number = distribution(generator);
+        printf("%i): %i\n",i ,(int)number);
+
         if (number < 10)
             ++p[int(number)];
     }
 
-    cout << "weibull_distribution (2.0,4.0):" << endl;
+    cout << "\nWeibull Distribution Plot:" << endl;
 
     for (int i = 0; i < 10; ++i)
     {
         cout << i << "-" << (i + 1) << ": ";
-        cout << string(p[i] * nstars / nrolls, '*') << endl;
+        cout << string(p[i] * nstars / iterations, '*') << endl;
     }
 
     return 0;
