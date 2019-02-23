2670548a0c521cf3e36a50b5b54b5183f2bdeb37
Eduardo Larios <eduardo.larios.fdz@outlook.com>
2019-02-23 16:18:56 -0600

    added montecarlo pi estimate

---
+++ Montecarlo Pi/header.h
@@ -0,0 +1,31 @@
+#ifndef TIMER_H_
+#define TIMER_H_
+
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/types.h>
+
+struct timeval startTime, stopTime;
+int started = 0;
+
+void start_timer() {
+	started = 1;
+	gettimeofday(&startTime, NULL);
+}
+
+double stop_timer() {
+	long seconds, useconds;
+	double duration = -1;
+
+	if (started) {
+		gettimeofday(&stopTime, NULL);
+		seconds  = stopTime.tv_sec  - startTime.tv_sec;
+		useconds = stopTime.tv_usec - startTime.tv_usec;
+		duration = (seconds * 1000.0) + (useconds / 1000.0);
+		started = 0;
+	}
+	return duration;
+}
+
+#endif /* TIMER_H_ */
